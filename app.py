f59bd5c77c8fd012e1da17650922da8521a37333
Natalia Kuzmina <kuzmina.nd@phystech.edu>
2022-05-07 12:28:58 +0300

    deal with parent

--- app.py
+++ app.py
@@ -16,18 +16,45 @@ SecondFormUI, SecondFormBase = load_form('form_two.ui')
 class MainWindow(MainForm):
     def __init__(self, parent=None):
         # Initialize base form:
-        super().__init__()
+        super().__init__(parent)
         # Create and initialize UI elements:
         self.ui = MainFormUI()
         self.ui.setupUi(self)
+        self.__current_form = None
+        #start with form 1
+        self.replace_form(FirstForm.get_instance())
 
     def __del__(self):
         self.ui = None
 
+    def replace_form(self, new_form):
+        # Replace and orpham current form if it's present
+        if self.__current_form is None:
+            self.ui.grid.addWidget(new_form)
+        else:
+            self.ui.grid.replaceWidget(self.__current_form, new_form)
+            self.__current_form.setParent(None)
+        # Set parent of a new form, update main window title, safe reference
+        self.setWindowTitle(new_form.windowTitle())
+        new_form.setParent(self)
+        self.__current_form = new_form
+
+
 class FirstForm(FirstFormBase):
+    __instance = None
+
+    @classmethod
+    def get_instance(cls):
+        if cls.__instance is None:
+            cls.__instance = cls()
+        return cls.__instance
+
     def __init__(self, parent=None):
+        # Singleton pattern
+        if type(self).__instance is not None:
+            raise Exception('attempt to create second instance')
         # Initialize base form:
-        super().__init__()
+        super().__init__(parent)
         # Create and initialize UI elements:
         self.ui = FirstFormUI()
         self.ui.setupUi(self)
@@ -38,10 +65,22 @@ class FirstForm(FirstFormBase):
         self.ui = None
 
     def __switch_to_form_two(self):
-        return
+        parent = self.parent()
+        parent.replace_form(SecondForm.get_instance())
 
 class SecondForm(SecondFormBase):
+    __instance = None
+
+    @classmethod
+    def get_instance(cls):
+        if cls.__instance is None:
+            cls.__instance = cls()
+        return cls.__instance
+
     def __init__(self, parent=None):
+        #singleton pattern
+        if type(self).__instance is not None:
+            raise Exception('attempt to create second instance')
         # Initialize base form:
         super().__init__()
         # Create and initialize UI elements:
@@ -54,7 +93,7 @@ class SecondForm(SecondFormBase):
         self.ui = None
 
     def __switch_to_form_one(self):
-        return
+        self.parent().replace_form(FirstForm.get_instance())
 
 def main():
     app = QApplication(sys.argv)
